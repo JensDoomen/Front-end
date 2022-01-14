ab890a68a25b00c2dc1ea8ee508007498bcf5104
Jens Doomen <457543@student.fontys.nl>
2022-01-14 11:36:22 +0100

    Oplevering ready

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,6 +1,6 @@
 <template>
   <form @submit.prevent="register">
-    <h1 class="h3 mb-3 fw-normal">Please Register</h1>
+    <h1 class="h3 mb-3 fw-normal" style="color:white;">Please Register</h1>
     <input v-model="name" type="text" class="form-control" placeholder="Name" required>
     <input v-model="email" type="email" class="form-control" placeholder="Email" required>
     <input v-model="password" type="password" class="form-control" placeholder="Password" required>
